1166726c4850d47339bd8f996fed63191f96b977
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2024-07-02 14:59:23 +0700

    Day1

---
+++ Day1/Hello.py
@@ -0,0 +1,26 @@
+
+
+# #comment tar wkwk
+# print(x)
+# print(y)
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# print(x)
+# print(y)
+# print(z)
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
+
+
